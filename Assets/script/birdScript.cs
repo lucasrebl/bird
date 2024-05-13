b54917a2b64aebedcb529553e9d5264aeaa43645
RLUCAS <rebouletlucas@gmail.com>
2024-05-13 16:44:21 +0200

    add scene game over

--- Assets/script/birdScript.cs
+++ Assets/script/birdScript.cs
@@ -6,17 +6,27 @@ public class birdScript : MonoBehaviour
 {
     public Rigidbody2D myRigidbody;
     public float flapStrength;
+    public logicScript logic;
+    public bool birdIsAlive = true;
+
+
     void Start()
     {
-        
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<logicScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) == true)
+        if (Input.GetKeyDown(KeyCode.Space) && birdIsAlive)
         {
             myRigidbody.velocity = Vector2.up * flapStrength;
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.gameOver();
+        birdIsAlive = false;
     }
 }
--- Assets/script/logicScript.cs
+++ Assets/script/logicScript.cs
@@ -2,16 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class logicScript : MonoBehaviour
 {
     public int playerScore;
     public Text scoreText;
+    public GameObject gameOverScreen;
 
     [ContextMenu("Augmenter le score")]
     public void addScore(int ScoreToAdd)
     {
         playerScore = playerScore + ScoreToAdd;
         scoreText.text = playerScore.ToString();
+    }
+
+    public void restartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void gameOver()
+    {
+        gameOverScreen.SetActive(true);
     }
 }
