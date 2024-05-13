c561ff4ab317e43e0a8866cd3910f0f2b729e68c
RLUCAS <rebouletlucas@gmail.com>
2024-05-13 16:00:15 +0200

    pipe spawn and destroy

---
+++ Assets/script/pipeMoveScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pipeMoveScript : MonoBehaviour
+{
+    public float moveSpeed = 5;
+    public float deathZone = -45;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = transform.position + (Vector3.left * moveSpeed) * Time.deltaTime;
+
+        if (transform.position.x < deathZone)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
